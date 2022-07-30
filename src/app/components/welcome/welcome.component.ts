9aba959abb09f8bc08d77d4498c5f45986c69098
Diego Otranto <diegui98@live.com.ar>
2022-07-29 23:58:48 -0300

    welcome component: randomdogs, pawloveImg, css

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DogsService } from 'src/app/services/dogs.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  randomDog1!: string;
+  randomDog2!: string;
 
-  constructor() { }
+  constructor(private dogsService: DogsService) {}
 
   ngOnInit() {
+    this.dogsService.getRandomDog().subscribe((data) => {
+      this.randomDog1 = data.message;
+    });
+    this.dogsService.getRandomDog().subscribe((data) => {
+      this.randomDog2 = data.message;
+    });
   }
-
 }
